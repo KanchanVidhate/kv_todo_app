9b31b367401bb2aa652ba018792141cfb5b373be
KanchanVidhate <kvidhate34@gmail.com>
2024-06-25 17:06:10 +0530

    addded

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -2,6 +2,8 @@ import "./Home.css"
 import AddIcon from "../Home/img/add.png"
 import ToDoCard from "./../../components/ToDoCard/ToDoCard"
 import { useState } from "react"
+import toast, { Toaster } from 'react-hot-toast';
+
 
 
 
@@ -32,13 +34,19 @@ function Home(){
          alt="Add" 
          className="icon-add"
          onClick={()=>{
+            if(newTask==""){
+                toast.error('Please Enter Task..')
+                return
+                                
+            }
             setTodoList([...todoList,newTask])
             setnewTask("")
+            toast.success('Task Added Succesfully..')
            
          }}
          />
       </div>
-
+        <Toaster position="top-center"/>
     </div>)
    
 }
